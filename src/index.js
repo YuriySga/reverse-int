0abd1452637387c141d094717ccd833c1c17f970
YuriySga <ess1928@yandex.ru>
2020-03-06 23:36:17 +0400

    feat: update reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {  
+  n = (n > 0) ? n : ~n + 1;
+  let arr = n.toString().split('');   
+  return Number(arr.reverse().join('')); 
 }
